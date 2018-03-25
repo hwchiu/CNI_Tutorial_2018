0b661b086605e0ec5a9f363d6b9b931aa7aab18d
hwchiu <hwchiu@linkernetworks.com>
2018-03-25 10:30:16 +0000

    add the simple confrig structure, including the bridge name and IP address

--- main.go
+++ main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
+
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/cni/pkg/version"
 )
 
+type SimpleBridge struct {
+	BrigdeName string `json:"bridgeName"`
+	IP string `json:"ip"`
+}
+
 func cmdAdd(args *skel.CmdArgs) error {
-	fmt.Println(args)
+	fmt.Println(args.ContainerID)
+	fmt.Println(args.Netns)
+	fmt.Println(args.IfName)
+	fmt.Println(args.Args)
+	fmt.Println(args.Path)
+
+	sb := SimpleBridge{}
+	if err := json.Unmarshal(args.StdinData, &sb); err != nil {
+		return err
+	}
+	fmt.Println(sb)
 	return nil
 }
 

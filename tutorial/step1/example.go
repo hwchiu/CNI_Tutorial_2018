4c7fddf6a169aa4662356585db3e7f4d89f601f3
hwchiu <hwchiu@linkernetworks.com>
2018-06-16 07:42:47 +0000

    Fix the run script of step1

--- tutorial/step1/example.go
+++ tutorial/step1/example.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/cni/pkg/version"
 )
 
 func cmdAdd(args *skel.CmdArgs) error {
+	fmt.Printf("interfance Name: %s\n", args.IfName)
+	fmt.Printf("netns path: %s\n", args.Netns)
+	fmt.Printf("the config data: %s\n", args.StdinData)
 	return nil
 }
 
--- tutorial/step1/run.sh
+++ tutorial/step1/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 go build -o example .
+echo "Ready to call the step1 example"
 sudo CNI_COMMAND=ADD CNI_CONTAINERID=ns1 CNI_NETNS=/var/run/netns/ns1 CNI_IFNAME=eth10 CNI_PATH=`pwd` ./example < config
+echo "The CNI has been called, see the following results"

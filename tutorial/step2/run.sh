cf68a51cd6aa6e774122cf501ecd95e1d2cf2f79
hwchiu <hwchiu@linkernetworks.com>
2018-06-16 07:42:57 +0000

    Fix the run script of step2

--- tutorial/step2/run.sh
+++ tutorial/step2/run.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+sudo ifconfig test down
+sudo brctl delbr test
 go build -o example .
+
+echo "Ready to call the step2 example"
 sudo CNI_COMMAND=ADD CNI_CONTAINERID=ns1 CNI_NETNS=/var/run/netns/ns1 CNI_IFNAME=eth10 CNI_PATH=`pwd` ./example < config
+echo "The CNI has been called, see the following results"
+echo "The bridge has been created"
+sudo brctl show test
